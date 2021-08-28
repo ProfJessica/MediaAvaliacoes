6b4349279e5f34a044f4d777a0f56acd4c83655f
ProfJessica <jessica.sgarcez@lideres.alicerceedu.com.br>
2021-08-28 15:41:54 -0300

    Exercício realizado

--- src/mediaavaliacoes/MediaAvaliacoes.java
+++ src/mediaavaliacoes/MediaAvaliacoes.java
@@ -1,21 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mediaavaliacoes;
 
-/**
- *
- * @author Alicerce Educação
- */
+//Implemente em Java, um código que leia a nota de 3 avaliações de um
+//aluno. A primeira avaliação tem peso 2, a segunda tem peso 3 e a
+//terceira, peso 5. Calcule a média do aluno. 
+
+//Se a média do aluno for maior ou igual a 7, o aluno está aprovado. 
+//Se a média do aluno for maior ou igual a 6 e menor que 7, o aluno irá para avaliação final. 
+//E se a média do aluno for menor que 6, o aluno está reprovado. 
+
+//Mostre o resultado das decisões. Para cálculo da média ponderada utilize:
+
+
 public class MediaAvaliacoes {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int n1 = 5; //peso 2
+        int n2 = 5; //peso 3
+        int n3 = 5; //peso 5
+        
+        int media = (n1*2 + n2*3 + n3*5)/10;
+        
+        // variavel1 && variavel2 -> E LÓGICO
+        // variavel || variavel2 -> OU LÓGICO
+        
+        
+        // media >= 7 ---> aprovado (if)
+        // media >= 6 && media < 7 ---> avaliação final (else if)
+        // media < 6 --> reprovado --> else
+        
+        if(media >= 7){
+            System.out.println("Sua média é: " + media + " Parabéns, você foi aprovado!!");
+        } else if (media >= 6 && media < 7){
+            System.out.println("Sua média é: " + media + " Ops!... Avaliação Final.");
+        } else {
+            System.out.println("Sua média é: " + media + "! Que pena, você foi reprovado.");
+        }
+        
     }
     
 }
